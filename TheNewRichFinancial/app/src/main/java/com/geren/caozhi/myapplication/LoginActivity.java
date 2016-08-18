61597aee69a37f163d3b961128672c284f9b37bf
unknown <farised@aliyun.com>
2016-08-18 21:08:33 +0800

    添加了login(登录)页面，但还未做完

---
+++ TheNewRichFinancial/app/src/main/java/com/geren/caozhi/myapplication/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.geren.caozhi.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/8/18.
+ */
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_view);
+    }
+}
