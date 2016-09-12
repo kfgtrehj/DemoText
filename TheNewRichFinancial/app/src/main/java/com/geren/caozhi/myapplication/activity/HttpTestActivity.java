207004b9806e468e548ece549ac8d58239cd1b23
neil <zhenghan@xfzb88.com>
2016-09-12 23:38:39 +0800

    add json parse demo

--- TheNewRichFinancial/app/src/main/java/com/geren/caozhi/myapplication/activity/HttpTestActivity.java
+++ TheNewRichFinancial/app/src/main/java/com/geren/caozhi/myapplication/activity/HttpTestActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.geren.caozhi.myapplication.R;
+import com.geren.caozhi.myapplication.bean.UpdateBean;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -18,21 +19,35 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
 public class HttpTestActivity extends AppCompatActivity {
 
     private Button requestBtn;
-    private TextView responseText;
+    private TextView responseText ,
+            version, //版本号
+            message, //信息
+            downloadUrl; // 下载地址
+
 
     final int RESPONSE = 0x01;
+    //线程之间的通信
     private Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             switch (msg.what){
                 case RESPONSE:
-                    String response = (String)msg.obj;
-                    responseText.setText(response);
+                    UpdateBean response = (UpdateBean) msg.obj;
+//                    responseText.setText("");
+                    if(response.getStatus() != 0){
+                        version.setText(response.getVersionCode());
+                        message.setText(response.getMessage());
+                        downloadUrl.setText(response.getDownloadUrl());
+                    }else {
+                        responseText.setText("当前已经是最新版本");
+                    }
+
                     break;
             }
 
@@ -44,6 +59,10 @@ public class HttpTestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_http_test);
         requestBtn = (Button) findViewById(R.id.requestBtn);
         responseText = (TextView) findViewById(R.id.responseText);
+        version = (TextView) findViewById(R.id.version);
+        message = (TextView) findViewById(R.id.message);
+        downloadUrl = (TextView) findViewById(R.id.downloadUrl);
+
         requestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,7 +79,7 @@ public class HttpTestActivity extends AppCompatActivity {
                 //创建一个HTTP 客户端 用于发起HTTP请求
                 HttpClient httpCient = new DefaultHttpClient();
                 //创建一个Get请求 url  = www.baidu.com
-                HttpGet httpGet = new HttpGet("http://www.baidu.com");
+                HttpGet httpGet = new HttpGet("https://m.sunfobank.com/ver/android/3.1.3");
 
                 try {
                     HttpResponse httpResponse = httpCient.execute(httpGet);
@@ -69,8 +88,22 @@ public class HttpTestActivity extends AppCompatActivity {
                         String response = EntityUtils.toString(entity,"utf-8");
                         Message message = new Message();
                         message.what = RESPONSE;
-                        message.obj = response.toString();
+//                        message.obj = response.toString();
+
+                        //对服务器传来的 json 文本进行解析
+                        //1创建一个Json 对象  把整个服务器传来的json 都放入 json 对象中
+                        JSONObject jsonObject = new JSONObject(response);
+                        //创建一个实体对象
+                        UpdateBean updateBean = new UpdateBean();
+                        //吧json对象中的 key 为 status 属性，映射到 对象 updateBean 里面的 status 属性中。
+                        updateBean.setStatus(jsonObject.getInt("status"));
+                        updateBean.setVersionCode(jsonObject.getString("version"));
+                        updateBean.setDownloadUrl(jsonObject.getString("downloadUrl"));
+                        updateBean.setMessage(jsonObject.getString("message"));
+                        //
+                        message.obj = updateBean;
                         handler.sendMessage(message);
+
                     }
 
                 }catch (Exception e){
@@ -79,5 +112,7 @@ public class HttpTestActivity extends AppCompatActivity {
             }
         }.start();
     }
+
+
 
 }
---
+++ TheNewRichFinancial/app/src/main/java/com/geren/caozhi/myapplication/bean/UpdateBean.java
@@ -0,0 +1,50 @@
+package com.geren.caozhi.myapplication.bean;
+
+/**
+ * Created by neil on 16/9/12.
+ */
+public class UpdateBean {
+
+    //最新版本号
+    private String versionCode;
+    //消息
+    private String message;
+    //状态
+    private int status;
+    //下载地址
+    private String downloadUrl;
+
+
+    public String getDownloadUrl() {
+        return downloadUrl;
+    }
+
+    public void setDownloadUrl(String downloadUrl) {
+        this.downloadUrl = downloadUrl;
+    }
+
+    public String getVersionCode() {
+        return versionCode;
+    }
+
+    public void setVersionCode(String versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+}
